4a1b7493c8eac45919208f0385243f38c2434c76
giamaicano <122812820+giamaicano@users.noreply.github.com>
2023-01-20 16:57:39 +0100

    Create main.cpp

---
+++ Auto-noleggio/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct g_s
+{
+    char l,ma,me,g,v,s,d;
+};
+
+struct auton{
+
+    string categoria,marca,modello,colore;
+    struct g_s gds;
+};
+
+
+int main()
+{
+
+
+
+
+    return 0;
+}
+
