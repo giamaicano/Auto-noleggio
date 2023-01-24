4f7f95c72db87de9bb7792bf49c29d857378d7f5
giamaicano <122812820+giamaicano@users.noreply.github.com>
2023-01-24 15:50:50 +0100

    Update main.cpp
    Scrittura della funzione per la ricerca (da sistemare) e altre piccole sistemazioni del menù

--- Auto-noleggio/main.cpp
+++ Auto-noleggio/main.cpp
@@ -9,8 +9,10 @@ using namespace std;
 struct auton{
 
     string categoria,marca,modello,colore;
+    //string giorni[7];
     string l,ma,me,g,v,s,d;
 };
+
 
 
 void vis_lista(auton an[])
@@ -21,7 +23,7 @@ void vis_lista(auton an[])
     while(getline(fin,app,','))
     {
       getline(fin,an[cont].categoria,',');
-      getline(fin,an[cont].marca,' ');
+      getline(fin,an[cont].marca,',');
       getline(fin,an[cont].modello,',');
       getline(fin,an[cont].colore,',');
       getline(fin,an[cont].l,',');
@@ -51,17 +53,124 @@ void vis_lista(auton an[])
 
     fin.close();
 }
-
-
-
-
-void stampa()
+void ricerca(auton an[])
 {
+    ifstream fin("Auto.csv");
+
+    string c;
+    int v = 1,b = 0,g[10],i = 1,k;
+    bool contr;
+
+    cout<<"Inserire il categoria di auto che si vuole noleggiare: ";
+    cin>>c;
+    cout<<"Inserire i giorni in cui la si vuole noleggiare(1,2,3,4,5,6,7)(8 per uscire): "<<endl;
+
+    do
+    {
+       cin>>g[i];
+       if(g[i]<8 && g[i]>-1)
+       i++;
+       else if(g[i]==8)
+        cout<<"Ricerca in corso..."<<endl;
+       else
+        cout<<"Numero sbagliato"<<endl;
+    }while(g[i]!=8 && i!=8);
 
 
+    cout<<"Auto disponibili: ";
+    while(an[v].categoria != "0")
+    {   k = 1; contr = false;
 
+        //cout<<an[v].categoria;
+        if(an[v].categoria == c)
+        {
 
+            while(g[k]!=8)
+            {
+                switch(g[k])
+                {
+                case 1:
+                    if(an[v].l == "L")
+                    contr = true;
+
+                    else{
+                    contr = false;
+                    k = 7;
+                    }
+                break;
+
+                case 2:
+                    if(an[v].ma == "L")
+                    contr = true;
+
+                    else{
+                    contr = false;
+                    k = 7;
+                    }
+                break;
+
+                 case 3:
+                    if(an[v].me == "L")
+                    contr = true;
+
+                    else{
+                    contr = false;
+                    k = 7;
+                    }
+                break;
+
+                case 4:
+                    if(an[v].g == "L")
+                    contr = true;
+
+                    else{
+                    contr = false;
+                    k = 7;
+                    }
+                break;
+                case 5:
+                    if(an[v].v == "L")
+                    contr = true;
+
+                    else{
+                    contr = false;
+                    k = 7;
+                    }
+                break;
+
+                case 6:
+                    if(an[v].s == "L")
+                    contr = true;
+
+                    else{
+                    contr = false;
+                    k = 7;
+                    }
+                break;
+                case 7:
+                    if(an[v].d == "L")
+                    contr = true;
+
+                    else{
+                    contr = false;
+                    k = 7;
+                    }
+                break;
+
+                }
+             k++;
+            }
+                if(contr == true)
+                //b++;
+                cout<<b<<")"<<an[v+1].categoria<<setw(3);
+
+        }
+     v++;
+    }
+
+    fin.close();
 }
+
 
 
 void menu()
@@ -87,7 +196,7 @@ void menu()
                     break;
 
                 case 2:
-
+                ricerca(an);
                     break;
 
 
@@ -96,7 +205,7 @@ void menu()
 
 
 
-    }while(sc!=3);
+    }while(sc!=4);
 
 
 
